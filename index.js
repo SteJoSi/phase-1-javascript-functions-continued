784e23c720e5c5675af60f0b70aba6875db9f21f
Stephanie Sison <sjsison27@icloud.com>
2022-05-19 20:00:44 -0700

    commit tree 3ad6292c6b1843da0909792783fcbcfab5398cd9

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(job = "go to the office") {
+    return  `This Monday, I will ${job}.`
+}
+
+function wrapAdjective(flare = "*") {
+    return function(msg = "special") {
+        return `You are ${flare}${msg}${flare}!`
+    }
+}
+
